122d8965208943176fae0eaa31c6411cfeec2acc
Henkomannen <35594537+Henrikolofsson@users.noreply.github.com>
2018-04-12 18:46:41 +0200

    Test

--- src/Server/ServerListener.java
+++ src/Server/ServerListener.java
@@ -1,16 +1,12 @@
 package Server;
 
-
-import javax.sql.ConnectionEventListener;
-
-import Server.Client.Connectionhandler;
 import Client.Message;
 
 public interface ServerListener {
 
 	void MessageReciver(Message m);
 
-	void userConnected(User user,ConnectionEventListener connectionhandler);
+//	void userConnected(User user,ConnectionEventListener connectionhandler);
 
 	void userDisconnected(User user);
 
